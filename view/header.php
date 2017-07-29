5f6ad11a2c4fed6dd4bcb17c9c1ece4b9905911b
sa947 <sa947@njit.edu>
2017-07-29 12:25:58 -0400

    Added header

---
+++ view/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>My Guitar Shop</title>
+        <link rel="stylesheet" type="text/css"
+	          href="/book_apps/ch05_guitar_shop/main.css">
+		  </head>
+
+
+		  <body>
+		  <header><h1>My Guitar Shop</h1></header>
+
